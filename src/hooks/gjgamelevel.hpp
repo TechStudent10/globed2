c6995cccc955aa30d0d4139bd1311a0e26a65290
shloakmenon <58670749+ninXout@users.noreply.github.com>
2024-02-28 14:28:32 -0500

    Add teleport to player button for moderators

--- src/hooks/gjgamelevel.hpp
+++ src/hooks/gjgamelevel.hpp
@@ -4,4 +4,8 @@
 // revolutionary
 class $modify(HookedGJGameLevel, GJGameLevel) {
     bool shouldTransitionWithPopScene = false;
+    bool shouldStopProgress = false; // stop progressing when using a cheat
+
+    $override
+    void savePercentage(int, bool, int, int, bool);
 };
